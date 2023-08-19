484bc6728633b1dc683bdc84f8adcb1992f04fa5
OmarMWarraich <earthling986@gmail.com>
2023-08-19 19:56:35 +0500

    feat: Migrate, StripePay View, Show Sesions' items

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -3,18 +3,19 @@ class CheckoutsController < ApplicationController
 
     def show
         current_user.set_payment_processor :stripe
-        current_user.set_payment_processor.customer
-
+        current_user.payment_processor.customer
+    
         @checkout_session = current_user
-            .payment_processor
-            .checkout(
-                mode: 'payment',
-                line_items: 'price_1NglYgGIQ4cRFDensQgPguna',
-                success_url: checkout_success_url
-            ) 
-    end
+                            .payment_processor
+                            .checkout(
+                              mode: 'payment',
+                              line_items: 'price_1NglYgGIQ4cRFDensQgPguna',
+                              success_url: checkout_success_url
+                            )
+      end
 
     def success
-
+        @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+        @line_items = Stripe::Checkout::Session.list_line_items(params[:session_id])
     end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,5 @@
 class PagesController < ApplicationController
   def home
-    return unless current_user
-
-    @portal_session = current_user.payment_processor.billing_portal
   end
 
   def about
