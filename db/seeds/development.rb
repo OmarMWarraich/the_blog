e721f9e0bfb0177bfb0e562e42cc8eb8a44ed4fa
OmarMWarraich <earthling986@gmail.com>
2023-08-24 18:32:26 +0500

    feat: Seed

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -6,36 +6,48 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 puts 'Seeding development database...'
-omar = User.first_or_create!(email: 'omar@example.com',
-                             password: 'password',
-                             password_confirmation: 'password',
-                             first_name: 'Omar',
-                             last_name: 'Warraich',
-                             role: User.roles[:admin])
+def seed_users
+  omar = User.create(email: 'omar@example.com',
+    password: 'password',
+    password_confirmation: 'password',
+    first_name: 'Omar',
+    last_name: 'Warraich',
+    role: User.roles[:admin])
 
-john = User.first_or_create!(email: 'john@doe.com',
-                             password: 'password',
-                             password_confirmation: 'password',
-                             first_name: 'John',
-                             last_name: 'Doe')
-Address.first_or_create!(street: '123 Main St',
-                         city: 'Anytown',
-                         state: 'CA',
-                         zip: '12345',
-                         country: 'USA',
-                         user: omar)
-Address.first_or_create(street: '123 Main St',
-                        city: 'Anytown',
-                        state: 'CA',
-                        zip: '12345',
-                        country: 'USA',
-                        user: john)
-category = Category.first_or_create!(name: 'Uncategorized', display_in_nav: true)
-Category.first_or_create!(name: 'Ruby', display_in_nav: false)
-Category.first_or_create!(name: 'Rails', display_in_nav: true)
-Category.first_or_create!(name: 'JavaScript', display_in_nav: true)
-elapsed = Benchmark.measure do
+  john = User.create(email: 'john@doe.com',
+    password: 'password',
+    password_confirmation: 'password',
+    first_name: 'John',
+    last_name: 'Doe')
+end
+
+def seed_addresses
+  Address.create(street: '123 Main St',
+    city: 'Anytown',
+    state: 'CA',
+    zip: '12345',
+    country: 'USA',
+    user: User.first)
+  Address.create(street: '123 Main St',
+   city: 'Anytown',
+   state: 'CA',
+   zip: '12345',
+   country: 'USA',
+   user: User.second)
+end
+
+def seed_categories
+  Category.create(name: 'Uncategorized', display_in_nav: true)
+  Category.create(name: 'Ruby', display_in_nav: false)
+  Category.create(name: 'Rails', display_in_nav: true)
+  Category.create(name: 'JavaScript', display_in_nav: true)
+end
+
+def seed_posts_and_comments
   posts = []
+  omar =  User.first
+  john = User.second
+  category = Category.first
   10.times do |x|
     puts "Creating post #{x}"
     post = Post.new(title: "Title #{x}",
@@ -44,13 +56,53 @@ elapsed = Benchmark.measure do
                     category: category)
 
     5.times do |y|
-      puts "Creating comment #{y} for post #{x}"
+      puts "Creating comment #{y} for post #{x} with user #{john.email}"
       post.comments.build(body: "Comment #{y}",
                           user: john)
     end
+
     posts.push(post)
   end
   Post.import(posts, recursive: true)
+end
+
+def seed_ahoy
+  Ahoy.geocode = false
+  request = OpenStruct.new(
+    params: {},
+    referer: 'http://example.com',
+    remote_ip: '0.0.0.0',
+    user_agent: 'Internet Explorer, lol can you imagine?',
+    original_url: 'rails'
+  )
+
+  visit_properties = Ahoy::VisitProperties.new(request, api: nil)
+  properties = visit_properties.generate.select { |_, v| v }
+
+  example_visit = Ahoy::Visit.create!(properties.merge(
+                                        visit_token: SecureRandom.uuid,
+                                        visitor_token: SecureRandom.uuid
+                                      ))
+
+  2.months.ago.to_date.upto(Date.today) do |date|
+    Post.all.each do |post|
+      rand(1..5).times do |_x|
+        Ahoy::Event.create!(name: 'Viewed Post',
+                            visit: example_visit,
+                            properties: { post_id: post.id },
+                            time: date.to_time + rand(0..23).hours + rand(0..59).minutes)
+      end
+    end
+  end
+end
+
+elapsed = Benchmark.measure do
+  puts 'Seeding development database...'
+  seed_users
+  seed_addresses
+  seed_categories
+  seed_posts_and_comments
+  seed_ahoy
 end
 
 puts "Seeded development DB in #{elapsed.real} seconds"
