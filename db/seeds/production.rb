9088993a4535f7d8ed54de20a42b5fb8850ecbf9
OmarMWarraich <earthling986@gmail.com>
2024-04-11 22:35:39 +0500

    fix: Production Admin Seeders

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,10 +1,21 @@
-def seed_users
-  omar = User.create(email: ENV['ADMIN_EMAIL'],
+puts 'Seeding production database...'
+
+# Create the admin user
+admin_user = User.create(
+  email: ENV['ADMIN_EMAIL'],
   password: ENV['ADMIN_PASSWORD'],
   password_confirmation: ENV['ADMIN_PASSWORD'],
   first_name: ENV['ADMIN_FIRST_NAME'],
   last_name: ENV['ADMIN_LAST_NAME'],
-  role: User.roles[:admin])
-end
-puts 'Seeding production database...'
-seed_users
+  role: 'admin'
+)
+
+# Create an address associated with the admin user
+Address.create(
+  street: '123 Main St',
+  city: 'Anytown',
+  state: 'CA',
+  zip: '12345',
+  country: 'USA',
+  user: admin_user
+)
