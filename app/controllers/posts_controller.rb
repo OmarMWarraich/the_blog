ae03302b356c4f56f82a48b758a14349fc14f21e
OmarMWarraich <earthling986@gmail.com>
2024-04-11 13:31:24 +0500

    feat: Drag and Drop Stimulus Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: %i[show index]
   # GET /posts or /posts.json
   def index
-    @posts = Post.all.includes(:user, :rich_text_body).order(created_at: :desc)
+    @posts = Post.all.includes(:user, :rich_text_body, :blob, :images_attachment).order(created_at: :desc)
   end
 
   # GET /posts/1 or /posts/1.json
@@ -76,7 +76,7 @@ class PostsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def post_params
-    params.require(:post).permit(:title, :body, :category_id, images)
+    params.require(:post).permit(:title, :body, :category_id, images: [])
   end
 
   def mark_notifications_as_read
