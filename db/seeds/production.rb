afc9b5dbbed60792f4cb71732221a2e2465ef1fb
OmarMWarraich <earthling986@gmail.com>
2024-04-11 21:26:20 +0500

    fix: N+1 queries

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,1 +1,10 @@
+def seed_users
+  omar = User.create(email: ENV['ADMIN_EMAIL'],
+  password: ENV['ADMIN_PASSWORD'],
+  password_confirmation: ENV['ADMIN_PASSWORD'],
+  first_name: ENV['ADMIN_FIRST_NAME'],
+  last_name: ENV['ADMIN_LAST_NAME'],
+  role: User.roles[:admin])
+end
 puts 'Seeding production database...'
+seed_users
